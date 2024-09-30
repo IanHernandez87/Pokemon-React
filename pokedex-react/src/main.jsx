c4f4a9a08642900bf5fd07ffaa07e3f4f94bfb8a
IanHernandez87 <ianh8902@gmail.com>
2024-09-30 22:35:36 +0200

    importar recursos

--- pokedex-react/src/main.jsx
+++ pokedex-react/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'animate.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
