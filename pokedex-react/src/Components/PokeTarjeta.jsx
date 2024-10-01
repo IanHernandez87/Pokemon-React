87be166994487da388a94897bd4ce6eccbaca2ab
IanHernandez87 <ianh8902@gmail.com>
2024-10-01 04:23:34 +0200

    poketarjeta y service que me extrae los detalles

--- pokedex-react/src/Components/PokeTarjeta.jsx
+++ pokedex-react/src/Components/PokeTarjeta.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Col, Card, CardBody, CardImg, Badge, CardFooter } from "reactstrap";
+import pokemonService from '../Views/pokemonService'; 
+import { Link } from 'react-router-dom';
 
-const PokeTarjeta = () => {
+const PokeTarjeta = ({ pok }) => { 
+  const [pokemon, setPokemon] = useState({});
+  const [imagen, setImagen] = useState('');
+  const [loading, setLoading] = useState(true); // Estado para manejar la carga
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    try {
+      const data = await pokemonService.getPokemonDetail(pok.url); 
+      setPokemon(data);
+      if(data.sprites.other.dream_world.front_default != null){
+        setImagen(data.sprites.other.dream_world.front_default); 
+      } else{
+        setImagen(data.sprites.other['official-network'].front_default); 
+      }
+    } catch (error) {
+      console.error("Error al obtener los detalles del Pokémon:", error);
+    } finally {
+      setLoading(false); 
+    }
+  };
+
   return (
-    <div>PokeTarjeta</div>
-  )
+    <Col sm='4' lg='3' className='mb-3'>
+      {loading ? ( 
+        <Card className='shadow border-4 border-warning'>
+          <CardImg src='/img/loading.gif' height='200' className='p-3' />
+        </Card>
+      ) : ( 
+        <Card className='shadow border-4 border-warning'>
+          <CardImg src={imagen} height='150' className='p-2' />
+          <CardBody className='text-center'>
+            <Badge pill color='danger'>#{pokemon.id}</Badge>
+            <label className='fs-4 text-capitalize'>{pokemon.name}</label>
+          </CardBody>
+          <CardFooter className='bg-warning d-flex justify-content-center'>
+            <Link className='btn btn-dark'>
+              <i className='fa-solid fa-arrow-up-right-from-square'></i> Detalle
+            </Link>
+          </CardFooter>
+        </Card>
+      )}
+    </Col>
+  );
 }
 
-export default PokeTarjeta
+export default PokeTarjeta;
--- pokedex-react/src/Views/pokemonService.jsx
+++ pokedex-react/src/Views/pokemonService.jsx
@@ -2,16 +2,28 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
+// Función para obtener la lista de Pokémon
 const getPokemones = async (limit, offset) => {
   try {
     const response = await axios.get(`${BASE_URL}?limit=${limit}&offset=${offset}`);
     return response.data.results; 
   } catch (error) {
-    console.error("Error al obtener los Pokémon::", error);
+    console.error("Error al obtener los Pokémon:", error);
     throw error; 
   }
 };
 
+const getPokemonDetail = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data; 
+  } catch (error) {
+    console.error("Error al obtener los detalles del Pokémon:", error);
+    throw error;
+  }
+};
+
 export default {
-  getPokemones, 
+  getPokemones,
+  getPokemonDetail, // Añadimos esta función para obtener detalles individuales
 };
